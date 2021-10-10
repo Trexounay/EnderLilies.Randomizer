5a7ccb414122c5361d106dad9f9dd600ad9f634c
Trex <trex@piranaking.com>
2021-10-10 09:48:38 +0200

    _

--- EnderLilies.Randomizer/EnderLiliesFactory.cs
+++ EnderLilies.Randomizer/EnderLiliesFactory.cs
@@ -11,7 +11,7 @@ namespace EnderLilies.Randomizer
         public string ComponentName => "Randomizer for Ender Lilies";
         public string Description => "A randomizer for ENDER LILIES: Quietius of the Knights";
         public ComponentCategory Category => ComponentCategory.Control;
-        public Version Version => Version.Parse("2.0.4");
+        public Version Version => Version.Parse("2.0.3");
 
         public string UpdateName => ComponentName;
         public string UpdateURL => "https://raw.githubusercontent.com/Trexounay/EnderLilies.Randomizer/master/Update/";
