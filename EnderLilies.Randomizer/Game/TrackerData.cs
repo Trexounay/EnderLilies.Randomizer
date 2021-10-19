a1fc31632645c2a5a766a3206fee3c8db00bdcbf
Trex <trex@piranaking.com>
2021-10-19 12:28:56 +0200

    test for tracker

---
+++ EnderLilies.Randomizer/Game/TrackerData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnderLilies.Randomizer
+{
+    struct TrackedItem
+    {
+    }
+
+    class TrackerData
+    {
+        Dictionary<string, TrackedItem> items = new Dictionary<string, TrackedItem>()
+        {
+            { "stone_tablet", new TrackedItem()},
+        };
+    }
+}
